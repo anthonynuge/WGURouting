2891ae7a3370e501d481b8b22101881f7e54f914
Anthony Nguyen <anthonynuge0509@gmail.com>
2023-12-31 17:22:07 -0600

    Create method to insert packages into hashtable.

--- wguRouting/router/HashTable.py
+++ wguRouting/router/HashTable.py
@@ -8,3 +8,16 @@ class HashTable:
         print("Hash Table:")
         for i, j in enumerate(self.table):
             print(f"Index:{i} ", j)
+
+    # Calculate and return index of table to store
+    def calcHash(self, id):
+        return id % self.size
+
+    # Add obj to table.
+    def insert(self, id, obj):
+        hashVal = self.calcHash(id)
+        # If index of table is empty add array list to store obj and future objs
+        if self.table[hashVal] is None:
+            self.table[hashVal] = []
+        # Add obj to array list
+        self.table[hashVal].append((id, obj))

4bdc3a2d4508584ab6c4129e2e37565ba5f6c5f6
Anthony Nguyen <anthonynuge0509@gmail.com>
2023-12-31 15:51:56 -0600

    Create package class.

---
+++ wguRouting/classes/Package.py
@@ -0,0 +1,33 @@
+from Status import Status
+
+
+class Package:
+    def __init__(self, id, address, city, state, zip, deadLine, weight, status):
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.deadLine = deadLine
+        self.weight = weight
+        self.status = Status(status)
+
+    def __str__(self) -> str:
+        return (
+            "id:%s, addr:%s, city:%s, state:%s, zip:%s, deadLine:%s, weight:%s, status:%s"
+            % (
+                self.id,
+                self.address,
+                self.city,
+                self.state,
+                self.zip,
+                self.deadLine,
+                self.weight,
+                self.status.value,
+            )
+        )
+
+
+if __name__ == "__main__":
+    test = Package(1, 312, "houston", "tx", 77040, 10, 4, Status.AT_HUB)
+    print(test)

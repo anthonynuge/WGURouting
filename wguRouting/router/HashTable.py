1e49383eb2b118e0650fe8e2138aa849992bddff
antho <anthonynuge0509@gmail.com>
2024-01-26 20:33:03 -0600

    Add documentation comments.

--- wguRouting/router/HashTable.py
+++ wguRouting/router/HashTable.py
@@ -10,21 +10,26 @@ class HashTable:
             print(f"Index:{i} ", j)
 
     # Calculate and return index of table to store
+    # O(1)
     def calcHash(self, id):
         return id % self.size
 
-    # Add obj to table.
+    # Add package object to the hash table. Calculates hash key using objects id.
+    # O(1)
     def insert(self, id, obj):
         hashVal = self.calcHash(id)
-        # If index of table is empty add array list to store obj and future objs
+        # If index of table is empty add array list to store obj and future objs with same hash value
         if self.table[hashVal] is None:
             self.table[hashVal] = []
         # Add obj to array list
         self.table[hashVal].append((id, obj))
 
-    # Checks if a package is in the hashtable based on id. Overall time complexity of O(n)
+    # Checks if a package is in the hashtable based on id.
+    # Best Case O(1).
+    # Overall time complexity of O(n) based on number of items in inner array
     def lookup(self, searchId):
         hashVal = self.calcHash(searchId)
+
         # Only iterates through inner array if there is a hash match. Helps with efficiency. O(1)
         if self.table[hashVal] is not None:
             # Checks each element in inner array time consuming part depending on number of obj collisions: O(n)

9b23bef46373e57e071203e4726041bbca00487c
antho <anthonynuge0509@gmail.com>
2024-01-26 20:26:12 -0600

    Refactor truck class.

--- wguRouting/classes/Truck.py
+++ wguRouting/classes/Truck.py
@@ -1,27 +1,27 @@
 class Truck:
-    def __init__(self, id):
+    def __init__(self, id, start, earliestDeparture):
         self.id = id
         self.distance = 0
         self.driver = None
         self.route = []
         self.speed = 18
         self.capacity = 16
-
-    # def getCurrentLocation(time):
-    #     pass
-    #
-    # def deliverPackage():
-    #     pass
-    #
-    # def returnToHub():
-    #     pass
-    #
-    # def add(package):
-    #     pass
-    #
+        self.currentLocation = start
+        self.earliestDeparture = earliestDeparture
 
     def hasRoom(self) -> bool:
         return len(self.route) <= self.capacity
 
     def loadPackage(self, packageId):
         self.route.append(packageId)
+
+    def __str__(self) -> str:
+        return (
+            # "id:%s\n, addr:%s\n, city:%s\n, state:%s\n, zip:%s\n, deadLine:%s\n, weight:%s\n, status:%s\n, specialNote:%s\n, earliestDeparture:%s\n, deliverWith:%s\n"
+            "Truck %s: \n Total Travel Distance: %s\n Route: %s\n"
+            % (
+                self.id,
+                self.distance,
+                self.route,
+            )
+        )

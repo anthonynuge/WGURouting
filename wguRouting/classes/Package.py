5739fd69f5d8b923b20660572ba11dca0be822dd
antho <anthonynuge0509@gmail.com>
2024-01-26 20:24:05 -0600

    Add method for updating package status. Refactor str and status attribute.

--- wguRouting/classes/Package.py
+++ wguRouting/classes/Package.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime, timedelta
 from .Status import Status
 
 
@@ -11,13 +12,14 @@ class Package:
         self.zip = zip
         self.deadLine = deadLine
         self.weight = weight
-        self.status = Status("at hub")
+        self.status = Status.AT_HUB
         self.specialNote = specialNote
         self.deliverWith = []
         self.delay = False
         self.earliestDeparture = None
+        self.timeDelivered = None
 
-        # If there is a special not parse and set attributes
+        # Parses special note into attributes for processing.
         if specialNote:
             if "Must be delivered with" in specialNote:
                 together = list(map(int, re.findall("\d+", specialNote)))
@@ -32,7 +34,9 @@ class Package:
 
     def __str__(self) -> str:
         return (
-            "id:%s, addr:%s, city:%s, state:%s, zip:%s, deadLine:%s, weight:%s, status:%s, specialNote:%s, earliestDeparture:%s, deliverWith:%s"
+            # "id:%s, addr:%s, city:%s, state:%s, zip:%s, deadLine:%s, weight:%s, status:%s, specialNote:%s, earliestDeparture:%s, deliverWith:%s"
+            "id:%s, addr:%s, city:%s, state:%s, zip:%s, deadLine:%s, weight:%s, status:%s, specialNote:%s"
+            # "id: %s\n addr: %s\n city: %s\n state: %s\n zip: %s\n deadLine: %s\n weight: %s\n status: %s\n specialNote: %s\n earliestDeparture:%s\n deliverWith: %s\n"
             % (
                 self.id,
                 self.address,
@@ -43,7 +47,16 @@ class Package:
                 self.weight,
                 self.status.value,
                 self.specialNote,
-                self.earliestDeparture,
-                self.deliverWith,
+                # self.earliestDeparture,
+                # self.deliverWith,
             )
         )
+
+    # Updates package status based on specific time. Status is updated only after routes are generated and package expectedDeliveryTime is set.
+    # O(1)
+    def updateStatus(self, checkedTime):
+        expectedDeliveryTime = datetime.strptime(self.timeDelivered, "%I:%M %p")
+        if checkedTime > expectedDeliveryTime:
+            self.status = Status.DELIVERED
+        elif checkedTime < expectedDeliveryTime:
+            self.status = Status.ENROUTE
